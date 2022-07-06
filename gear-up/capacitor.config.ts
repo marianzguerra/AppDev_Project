086bce53710f58b4460eaeeabecbdcc8592fd929
GuerraMarielZ <guerramarielz09@gmail.com>
2022-07-06 11:20:07 +0800

    mobile application folder

---
+++ gear-up/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Gear Up',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
