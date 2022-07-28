2699a1de363f13b04be18abafd5268fbfebc29a6
marianzguerra <marian.guerra@g.batstate-u.edu.ph>
2022-07-28 00:31:56 -0700

    logout toast

--- gear-up/src/app/app.component.ts
+++ gear-up/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,9 +10,20 @@ export class AppComponent {
   public appPages = [
     
   ];
-  constructor(public router:Router){}
+  constructor(public router:Router, public Toast: ToastController){}
   
   login(){
     this.router.navigate(['login']);
+    this.ShowMessage();
+  }
+
+  async ShowMessage(){
+    let toast = await this.Toast.create({
+      message: "You have been logout!!",
+      cssClass: "custom-toast",
+      duration: 2000,
+      position:'middle'
+    });
+    await toast.present();
   }
 }
